d8ec5e568ae477512e841c500f1e931ceb6d5a36
Alex Park <alexpark0930@gmail.com>
2024-04-05 11:30:47 -0700

    change rectangle to image sprite

--- src/main/java/ca/bcit/comp2522/termproject/comp2522202410termprojectjavafx/Player.java
+++ src/main/java/ca/bcit/comp2522/termproject/comp2522202410termprojectjavafx/Player.java
@@ -1,18 +1,22 @@
 package ca.bcit.comp2522.termproject.comp2522202410termprojectjavafx;
 
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class Player {
 
-    private final Rectangle shape;
+    private final ImageView shape;
 
     public Player(double x, double y, double width, double height) {
-        shape = new Rectangle(x, y, width, height);
-        shape.setFill(Color.BLUE);
+        Image playerImage = new Image("jetFighter.png"); // Replace "path_to_your_image_file.jpg" with the actual path to your image file
+        shape = new ImageView(playerImage);
+        shape.setX(x);
+        shape.setY(y);
+        shape.setFitWidth(width);
+        shape.setFitHeight(height);
     }
 
-    public Rectangle getShape() {
+    public ImageView getShape() {
         return shape;
     }
 
@@ -25,7 +29,7 @@ public class Player {
     }
 
     public double getWidth() {
-        return shape.getWidth();
+        return shape.getFitWidth();
     }
 
     public void moveLeft(double speed) {
